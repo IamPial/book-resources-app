b6b62fa1aeac982aede01502d4f3f5d42a3856cf
Pial <pialname394@gmail.com>
2026-04-06 03:33:33 +0600

    render the banner section from here

--- src/pages/homepages/HomePage.jsx
+++ src/pages/homepages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "../../components/shared/homepage/Banner";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
+      <Banner />
     </div>
   );
 };
