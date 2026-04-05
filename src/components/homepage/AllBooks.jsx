26763cd6469b53149da5fdfea16383760ca2e3a8
Pial <pialname394@gmail.com>
2026-04-06 03:55:37 +0600

    create books card section and render it through the map

---
+++ src/components/homepage/AllBooks.jsx
@@ -0,0 +1,22 @@
+import React, { use } from "react";
+
+import BookCard from "../ui/BookCard";
+const bookPromise = fetch("/booksData.json").then((res) => res.json());
+
+const AllBooks = () => {
+  const books = use(bookPromise);
+  return (
+    <div className="container mx-auto py-25">
+      <h2 className="font-playfair font-bold text-[40px] mb-9 text-center">
+        Books
+      </h2>
+      <div className="grid grid-cols-3 gap-6">
+        {books.map((book) => {
+          return <BookCard key={book.bookId} book={book} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AllBooks;
