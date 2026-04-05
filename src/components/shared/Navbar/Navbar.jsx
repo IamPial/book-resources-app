175d9e78eee83e32d874e15628d8b2fac030cb40
Pial <pialname394@gmail.com>
2026-04-06 02:04:00 +0600

    putted a gap between the nav links into small screen

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow space-y-2"
             >
               {links}
             </ul>
