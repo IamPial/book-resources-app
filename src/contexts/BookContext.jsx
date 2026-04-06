22d82bd9060ddf1941e7ca297fad230f890993cf
Pial <pialname394@gmail.com>
2026-04-07 01:10:33 +0600

    create handleWishList func for wish list button

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,18 +1,39 @@
 import React, { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 export const AllBookContext = createContext();
 
 const BookContext = ({ children }) => {
   const [storeBook, setStoreBook] = useState([]);
+  const [wishList, setWishList] = useState([]);
 
   const handleBookReadBtn = (currentBook) => {
     const isExist = storeBook.find(
       (book) => book.bookId === currentBook.bookId,
     );
     if (isExist) {
-      alert("The book is already exist");
+      toast.error("The book is already exist");
     } else {
+      toast.success(`${currentBook.bookName} is added to the read list`);
       setStoreBook([...storeBook, currentBook]);
+    }
+  };
+
+  const handleWishListBtn = (currentBook) => {
+    const isExistReadBook = storeBook.find(
+      (book) => book.bookId === currentBook.bookId,
+    );
+    if (isExistReadBook) {
+      toast.error("Already the book is exist in read list");
+      return;
+    }
+
+    const isExist = wishList.find((book) => book.bookId === currentBook.bookId);
+    if (isExist) {
+      toast.error("The book is already exist");
+    } else {
+      toast.success(`${currentBook.bookName} is added to the wishlist`);
+      setWishList([...wishList, currentBook]);
     }
   };
 
@@ -20,6 +41,9 @@ const BookContext = ({ children }) => {
     handleBookReadBtn,
     storeBook,
     setStoreBook,
+    handleWishListBtn,
+    wishList,
+    setWishList,
   };
 
   return (
