7083d5f411ad36495871ff56eea978cdb8ad18ba
Pial <pialname394@gmail.com>
2026-04-07 00:49:51 +0600

    creating books components for all read books

--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AllBookContext } from "../../contexts/BookContext";
 
 const Books = () => {
+  const { storeBook } = useContext(AllBookContext);
+  console.log(storeBook, "storeBook");
   return (
     <div>
-      <h1>Books</h1>
+      <h1 className="text-2xl bg-red-600">Books</h1>
     </div>
   );
 };
