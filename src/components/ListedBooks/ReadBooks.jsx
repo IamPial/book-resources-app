5572bd804685160fe54afe36eca39335913c6556
Pial <pialname394@gmail.com>
2026-04-07 03:18:49 +0600

    created readbook components

--- src/components/ListedBooks/ReadBooks.jsx
+++ src/components/ListedBooks/ReadBooks.jsx
@@ -1,12 +1,16 @@
 import React, { useContext } from "react";
+
 import { AllBookContext } from "../../contexts/BookContext";
+import Card from "../ui/Card";
 
 const ReadBooks = () => {
   const { readList } = useContext(AllBookContext);
   console.log(readList, "storeBook");
   return (
-    <div>
-      <h2>Read Books</h2>
+    <div className="mt-8 px-2 md:px-0">
+      {readList.map((obj, index) => {
+        return <Card obj={obj} key={index} />;
+      })}
     </div>
   );
 };
