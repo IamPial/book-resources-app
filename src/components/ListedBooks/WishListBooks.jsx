a9316521f553f0dacb3fafe93c40618c3823a0e9
Pial <pialname394@gmail.com>
2026-04-07 03:19:21 +0600

    created wistlist components

--- src/components/ListedBooks/WishListBooks.jsx
+++ src/components/ListedBooks/WishListBooks.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { AllBookContext } from "../../contexts/BookContext";
+import Card from "../ui/Card";
 
 const WishListBooks = () => {
   const { wishList } = useContext(AllBookContext);
   console.log(wishList, "wishlist");
   return (
-    <div>
-      <h2>WishList Books</h2>
+    <div className="mt-8 px-2 md:px-0">
+      {wishList.map((obj, index) => {
+        return <Card obj={obj} key={index} />;
+      })}
     </div>
   );
 };
