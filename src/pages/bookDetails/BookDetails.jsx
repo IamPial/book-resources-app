2e00ff34c81ed537455a449f77936863667bbd58
Pial <pialname394@gmail.com>
2026-04-06 17:58:09 +0600

    create book details into this section

--- src/pages/bookDetails/BookDetails.jsx
+++ src/pages/bookDetails/BookDetails.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
 
-//  {
-//     "bookId": 1,
-//     "bookName": "The Great Gatsby",
-//     "author": "F. Scott Fitzgerald",
-//     "image": "https://i.ibb.co.com/khHN7Pk/9780143454212.jpg",
-//     "review": "'The Great Gatsby' by F. Scott Fitzgerald is a timeless masterpiece that delves into the decadence and disillusionment of the Jazz Age. Set in the Roaring Twenties, the novel unveils the enigmatic Jay Gatsby's extravagant parties, masking a pursuit of lost love. Narrated by Nick Carraway, the story explores themes of wealth, love, and the American Dream, drawing readers into a vivid portrayal of the glittering yet elusive world of the East and West Egg. Fitzgerald's prose is both poetic and haunting, weaving a compelling narrative that transcends its era. A poignant exploration of societal excess and the human condition, 'The Great Gatsby' remains a literary gem that resonates across generations.",
-//     "totalPages": 192,
-//     "rating": 4.5,
-//     "category": "Classic",
-//     "tags": ["Fiction", "Romance"],
-//     "publisher": "Scribner",
-//     "yearOfPublishing": 1925
-//   },
-
 const BookDetails = () => {
   const { id } = useParams();
-
   const books = useLoaderData();
   const findBook = books.find((book) => book.bookId === Number(id));
 
   const {
-    bookId,
     bookName,
     author,
     image,
@@ -37,11 +21,11 @@ const BookDetails = () => {
 
   return (
     <div className="container mx-auto pt-12">
-      <div className="card gap-12 lg:card-side bg-base-100 shadow-sm">
-        <figure className="flex-1  p-15 rounded-xl bg-red-200">
+      <div className="card gap-12 lg:card-side bg-base-100">
+        <figure className="flex-1  p-15 rounded-xl bg-[#f3f3f3]">
           <img src={image} alt={bookName} />
         </figure>
-        <div className="card-body flex-1">
+        <div className="card-body flex-1 ">
           <div>
             <h2 className="font-bold text-[40px] font-playfair">{bookName}</h2>
             <p className="font-medium text-xl opacity-80 ">By : {author}</p>
@@ -51,10 +35,64 @@ const BookDetails = () => {
             <p className="font-medium text-xl opacity-80">{category}</p>
           </div>
           <div className="w-full border  border-gray-200 "></div>
-          <p>{review}</p>
-
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+          <p className="text-[16px] leading-7 grow-0 text-gray-600 py-6">
+            {" "}
+            <span className="font-bold  text-[16px] text-slate-950">
+              {" "}
+              Review:
+            </span>{" "}
+            {review}
+          </p>
+          <div className="pb-6">
+            {" "}
+            <span className="text-[16px] font-bold mr-5">Tag</span>
+            {tags.map((tag, index) => {
+              return (
+                <div
+                  key={index}
+                  className="badge  rounded-full text-[16px] font-medium text-[#23BE0A] bg-[#f4fcf3]"
+                >
+                  #{tag}
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-full border  border-gray-200 "></div>
+          <div className=" my-6 ">
+            <div className="flex  p-2">
+              <p className="w-1/2 text-[16px] text-gray-600">
+                Number of Pages:
+              </p>
+              <h2 className="w-1/2 font-semibold text-[16px] text-slate-950">
+                {totalPages}
+              </h2>
+            </div>
+            <div className="flex  p-2">
+              <p className="w-1/2 text-[16px] text-gray-600">Publisher:</p>
+              <h2 className="w-1/2 font-semibold text-[16px] text-slate-950">
+                {publisher}
+              </h2>
+            </div>
+            <div className="flex  p-2">
+              <p className="w-1/2 text-[16px] text-gray-600">
+                Year of Publishing:
+              </p>
+              <h2 className="w-1/2 font-semibold text-[16px] text-slate-950">
+                {yearOfPublishing}
+              </h2>
+            </div>
+            <div className="flex p-2">
+              <p className="w-1/2 text-[16px] text-gray-600">Rating:</p>
+              <h2 className="w-1/2 font-semibold text-[16px] text-slate-950">
+                {rating}
+              </h2>
+            </div>
+          </div>
+          <div className="card-actions justify-start mt-10 gap-4">
+            <button className="btn btn-lg rounded-lg ">Read</button>
+            <button className="btn btn-lg text-white rounded-lg bg-[#50B1C9]">
+              Wishlist
+            </button>
           </div>
         </div>
       </div>
