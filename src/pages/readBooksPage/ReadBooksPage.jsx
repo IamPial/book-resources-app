7cad29a914a791da0627b3413d30ce569847ef93
Pial <pialname394@gmail.com>
2026-04-08 01:57:55 +0600

    added loader data for showing the books data

--- src/pages/readBooksPage/ReadBooksPage.jsx
+++ src/pages/readBooksPage/ReadBooksPage.jsx
@@ -1,3 +1,4 @@
+import { useLoaderData } from "react-router";
 import {
   BarChart,
   Bar,
@@ -14,56 +15,14 @@ const colors = [
   "#00C49F",
   "#FFBB28",
   "#FF8042",
-  "red",
-  "pink",
-  "black",
+  "#95008A",
+  "#794747",
+  "#E17DC2",
+  "#004d7a",
+  "#008793",
 ];
 
 // #region Sample data
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 // #endregion
 const getPath = (x, y, width, height) => {
@@ -97,16 +56,24 @@ const CustomColorLabel = (props) => {
 };
 
 const ReadBooksPage = () => {
+  const allBooksData = useLoaderData();
+  const formattedData = allBooksData.map((book) => ({
+    ...book,
+    shortName: book.bookName.slice(0, 10) + "...",
+  }));
+
   return (
     <BarChart
+      className="container mx-auto mt-10 "
       style={{
         width: "100%",
-        maxWidth: "700px",
+        maxWidth: "1300px",
         maxHeight: "70vh",
         aspectRatio: 1.618,
+        // aspectRatio: 2,
       }}
       responsive
-      data={data}
+      data={formattedData}
       margin={{
         top: 20,
         right: 0,
@@ -116,11 +83,12 @@ const ReadBooksPage = () => {
     >
       <CartesianGrid strokeDasharray="3 3" />
       <Tooltip cursor={{ fillOpacity: 0.5 }} />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="shortName" />
       <YAxis width="auto" />
-      <Bar dataKey="uv" fill="#8884d8" shape={TriangleBar} activeBar>
+      <Bar dataKey="totalPages" fill="#8884d8" shape={TriangleBar} activeBar>
         <LabelList content={CustomColorLabel} position="top" />
       </Bar>
+
       {/* <RechartsDevtools /> */}
     </BarChart>
   );
