c0b668ce0344ac876daa015342b9e4a0a265306f
Pial <pialname394@gmail.com>
2026-04-07 12:15:09 +0600

    readList section sortedData by filtering pages & ratings

--- src/components/ListedBooks/ReadBooks.jsx
+++ src/components/ListedBooks/ReadBooks.jsx
@@ -1,12 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { AllBookContext } from "../../contexts/BookContext";
 import Card from "../ui/Card";
 
-const ReadBooks = () => {
+const ReadBooks = ({ sortBy }) => {
   const { readList } = useContext(AllBookContext);
+  console.log(readList, "unsortedData");
+  const [filteredList, setFilteredList] = useState(readList);
 
-  if (readList.length === 0) {
+  useEffect(() => {
+    if (sortBy) {
+      if (sortBy === "pages") {
+        const sortedData = [...readList].sort(
+          (a, b) => a.totalPages - b.totalPages,
+        );
+        setFilteredList(sortedData);
+      } else if (sortBy === "rating") {
+        const sortedData = [...readList].sort((a, b) => {
+          a.rating - b.rating;
+        });
+        setFilteredList(sortedData);
+      }
+    }
+  }, [sortBy, readList]);
+
+  if (filteredList.length === 0) {
     return (
       <div className="flex items-center justify-center bg-base-300  min-h-[60vh] rounded-lg px-4 md:px-0">
         <h1 className="text-4xl font-bold text-center ">
@@ -18,7 +36,7 @@ const ReadBooks = () => {
 
   return (
     <div className="mt-8 px-2 md:px-0">
-      {readList.map((obj, index) => {
+      {filteredList.map((obj, index) => {
         return <Card obj={obj} key={index} />;
       })}
     </div>
